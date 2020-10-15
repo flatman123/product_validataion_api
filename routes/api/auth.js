8065addb0a3fff09f710a512fe893acbf0c3f981
Jeffrey McIntyre <RedEyeCoding@gmail.com>
2020-10-15 16:02:12 -0400

    added code for protected routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const user = require('../../routes/api/user');
+const user = require('../../models/Users');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const mongoose = require('mongoose');
 
 // GET @Route /api/auth
 
 router.get('/', auth, async function(req, res, next) {
-    // Code for succesful token authentication
+    // Code for succesful token authentication        
+    try {
+        // Fetch User from database
+        const userData = await user.findById(req.appUser.id);
+        res.json(userData);
+
+    } catch(err) {
+        // console.error(err.messages)
+        res.status(500).send('Server Error');
+    };
 });
+
+module.exports = router;
