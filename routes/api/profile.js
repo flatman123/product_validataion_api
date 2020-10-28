2d8553a045edd9fef085731ee76872f0fd45d225
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-10-27 21:39:47 -0400

    start protected route for creating / updating user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/Users');
+const { check, validationResult } = require('express-validator');
+
 
 
 // @Route /api/profile/me
@@ -10,19 +12,38 @@ const User = require('../../models/Users');
 // @Access Private
 
 router.get('/me', auth, async (req, res) => {
-
     try {
         //locate User Profile by ID within the in database
         const profile = await Profile.findOne( { userPro: req.body.id }).populate('user',
             ['name', 'avatar']);
 
         !profile ? res.status(400).json( { msg: 'There is no profile for that user in the database.' }) : null;
-        
-
     } catch(err) {
-        console.error(err.messages);
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
+});
+
+// @Route /api/profile/
+// desc Create and Update user profile
+// @Access Private
+
+router.post('/', [auth, [
+    check('amazonSellerType')
+        .isIn(['FBA', 'FBM'])
+        .not()
+        .isEmpty()        
+]], async (req, res) => {
+    // Code
+    const error = validationResult(req);
+    if (!error.isEmpty()){
+        return res.status(400).json({ msg: 'Amazon seller type is required' });
+    };
+
+    // Check for profile
+    
+
+
 });
 
 module.exports = router;
