ed5d7ac1b2eef93603ac11411e7745e67d187c03
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-03 22:10:13 -0500

    Added some more code

--- routes/api/productsInfo.js
+++ routes/api/productsInfo.js
@@ -2,16 +2,33 @@ const ProductInfo = require('../models/productInfoModel');
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const { json } = express.json();
+
 
 
 
 // GET /api/product-info/
 // @desc Get user products via id
 // @Accecs Private
-router.get('/my-product', [auth,
-    check('asin')
+router.get('/my-product/:userID', [auth,
+    check('asin', 'Product asin is required')
         .not()
-        .isEmpty()
+        .isEmpty(),
+    check('productName', 'A product name is required')
 ], async(req, res) => {
 
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        res.status(400).json({ error: error});
+    };
+
+    try {
+        // Get user product Via Id
+
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
