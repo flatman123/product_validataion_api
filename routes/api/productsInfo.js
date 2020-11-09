e178df641d8c8970971e065074eaa8ef5dad0e30
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-08 22:50:27 -0500

    added more code for productsInfo route

--- routes/api/productsInfo.js
+++ routes/api/productsInfo.js
@@ -24,19 +24,51 @@ router.get('/my-products/:userID', [auth,
         res.status(400).json({ error: error});
     };
 
+    // Build product Object
+    const {
+        asin,
+        productName,
+        link,
+        quantity,
+        functionality,
+        quality,
+        aesthetic
+    } = req.body;
+
+    productInfo = {};
+    productInfo.user = req.appUser.id;
+
+    if (asin) productInfo.asin = asin;
+    if (productName) productInfo.productInfo = productInfo;
+    if (link) productInfo.link = link;
+    if (quantity) productInfo.quantity = quantity;
+    if (functionality) productInfo.functionality = functionality;
+    if (quality) productInfo.quality = quality;
+    if (aesthetic) productInfo.aesthetic = aesthetic;
+
+
     try {
         // Get user product Via Id
-        const userProducts = await ProductInfo
-                                        .findOne({ user: req.params.userID })
-                                        .populate('user', ['name','avatar']);
-        if (!userProducts) {
-            return res.status(400)
-                      .send('There are no products on your list');
+        let userProducts = await ProductInfo
+            .findOne({ user: req.params.userID })
+            .populate('user', 'name');
+        
+        if (userProducts) {
+            await ProductInfo.findByIdAndUpdate(
+                { user: req.appUser.id },
+                { $set: productInfo },
+                { new: true }
+            );
+        };
 
-        }
+        await ProductInfo.save(productInfo);
+        res.json(productInfo);
+
 
     } catch(err) {
         console.error(err);
         res.status(500).send('Server Error');
-    }
+    };
 });
+
+module.exports = router;
